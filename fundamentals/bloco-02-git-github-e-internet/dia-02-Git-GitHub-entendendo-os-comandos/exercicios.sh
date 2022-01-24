108d107a4615cf9e3ca9d0cd46fd48ee6993e356
Maykon Freitas <maykon17programmer@gmail.com>
2022-01-24 19:10:27 -0300

    Atualizando o arquivo de exercicios

---
+++ fundamentals/bloco-02-git-github-e-internet/dia-02-Git-GitHub-entendendo-os-comandos/exercicios.sh
@@ -0,0 +1,11 @@
+# 1- Navegue até a raiz do projeto com o arquivo .txt;
+cd /
+
+#2 - Verifique se não existe nada sem "commitar" utilizando git status :
+    #Caso exista algo, verifique se é necessário e faça o commit , ou remova-o.
+git status
+
+#3 - Crie uma nova branch com o nome trybe-skills-changes e faça checkout nela;
+git checkout -b trybe-skills-changes
+
+#4 - No arquivo .txt , ao final da lista de habilidades , adicione mais duas habilidades que serão desenvolvidas na Trybe:
