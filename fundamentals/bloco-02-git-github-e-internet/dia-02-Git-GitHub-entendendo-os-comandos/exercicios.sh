8cc1def4c7028263a0fb6b57c90bc89d77206bf9
Maykon Freitas <maykon17programmer@gmail.com>
2022-01-25 10:29:02 -0300

    Atualizando o arquivo de exercicios

--- fundamentals/bloco-02-git-github-e-internet/dia-02-Git-GitHub-entendendo-os-comandos/exercicios.sh
+++ fundamentals/bloco-02-git-github-e-internet/dia-02-Git-GitHub-entendendo-os-comandos/exercicios.sh
@@ -39,5 +39,29 @@ git checkout main
 #7 - Verifique que você está na branch master , com o comando: git branch (esta branch deve estar com o formato original, sem as habilidades recém adicionadas);
 
 #8 - Crie uma nova branch trybe-skills-updates a partir da master e faça checkout nela;
+git checkout -b trybe-skills-updates
 
-#9 - No mesmo arquivo .txt que você modificou no passo 4 , também ao final da sua lista de habilidades, adicione mais um aprendizado que você terá nos próximos blocos:
+#9.0 - No mesmo arquivo .txt que você modificou no passo 4 , também ao final da sua lista de habilidades, adicione mais um aprendizado que você terá nos próximos blocos:
+cat >> trybe-skills.txt
+NodeJS
+
+#9.1 - Atenção! Aqui o arquivo não terá as alterações feitas anteriormente na outra branch 😉;
+
+#9.2 - Faça um git add nome-do-arquivo.extensao ;
+git add trybe-skills.txt
+
+#9.3 - Agora um git commit -m "Mensagem que você gostaria" ;
+git commit -m "Atualizando o arquivo trybe-skills"
+
+#9.4 - e por último um git push -u origin trybe-skills-updates ;
+git push -u origin trybe-skills-updates
+
+#9.5 - Após o primeiro "push" da sua branch, você pode usar apenas o comando git push ;
+#9.6 - Abra um Pull Request com uma descrição amigável:
+    #9.6.1 - O merge deve ser feito apenas quando chegar no exercício 10;
+
+#10.0 - Agora, faça o merge das branches trybe-skills-changes e trybe-skills-updates na branch master , através do Pull Request :
+#10.1 - Primeiro, vá até a página do primeiro Pull Request (branch trybe-skills-changes ) e faça o merge clicando no botão verde "Merge pull request" ;
+#10.2 - Agora, vá até página do outro Pull Request (branch trybe-skills-updates ) e tente fazer o merge clicando no mesmo botão. Reparou que ele está bloqueado? Isso acontece porque esse Pull Request está tentando alterar a mesma linha de código com um conteúdo diferente, e o Git não consegue determinar sozinho qual das duas linhas é a correta;
+#10.3 - Você terá que resolver esse conflito antes de "mergear" o Pull Request . Clique no botão "Resolve conflicts" , escolha uma das duas versões do texto (lembre-se de apagar as linhas com <<<<<<< e >>>>>>> , elas são criadas apenas para dar uma identificação mais visual ao problema) . Depois clique em "Mark as resolved" e, em seguida, em "Commit merge" ;
+#10.4 - Agora você deve conseguir "mergear" seu Pull Request normalmente. 😎
