bc04163900f67ce6a5e80d03123de28bea7cb847
Maykon Freitas <maykon17programmer@gmail.com>
2022-02-11 19:24:14 -0300

    Finalizando 2º questao do exercicio do Bloco 05 - Dia 03

--- fundamentals/bloco-05-javacript-DOM-Eventos-web-storage/dia-03-javascript-eventos/Exercises/script.js
+++ fundamentals/bloco-05-javacript-DOM-Eventos-web-storage/dia-03-javascript-eventos/Exercises/script.js
@@ -41,3 +41,32 @@ function setDaysInList(listDays, className) {
 
 // Chamando a função
 setDaysInList(dezDaysList, 'days');
+
+/**
+ * Exercício 2:
+		Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+		Adicione a este botão a ID "btn-holiday" .
+		Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+ */
+
+// Pegando o buttons-container
+let buttonsContainer = document.querySelector('.buttons-container');
+
+function createButtonChild(string) {
+	// Criando e configurando o Botão
+	let button = document.createElement('button');
+	button.id = 'btn-holiday';
+	button.innerText = string;
+	
+	// Adicionando ao pai
+	buttonsContainer.appendChild(button);
+}
+
+createButtonChild('Feriados');
+
+/**
+* 	Exercício 3:
+			Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+			É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+*/
+
