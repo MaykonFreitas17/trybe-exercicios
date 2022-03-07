1820c163a7fa24e5c55097f5801071090792c641
Maykon Freitas <maykon17programmer@gmail.com>
2022-03-07 20:18:01 -0300

    Finalizando o(s) exercicio(s) 1º e 2º - Bloco 07 - Dia 03

---
+++ fundamentals/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/test/sum.test.js
@@ -0,0 +1,26 @@
+const sum = require('../scripts/sum');
+
+/**
+ * 1 - A função sum(a, b) retorna a soma do parâmetro a com o b
+  -> Teste se o retorno de sum(4, 5) é 9
+  -> Teste se o retorno de sum(0, 0) é 0
+  -> Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+  -> Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+*/
+
+describe('1º exercicio - Função SUM', () => {
+  
+  it('Se sum(4, 5) é 9', () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  it('Se sum(0, 0) é 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  // it('Se sum(4, "5") é parameters must be numbers', () => {
+  //   expect(sum(4, '5')).toThrow(Error);
+  // });
+
+
+})
