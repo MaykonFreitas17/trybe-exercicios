680d92de6612cb122cf74fd6fb19c34cacb6cdbb
Maykon Freitas <maykon17programmer@gmail.com>
2022-03-11 19:13:35 -0300

    Finaliznado os exercicio do Bloco 08 - Dia 03

---
+++ fundamentals/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/Exercises/exercise7.js
@@ -0,0 +1,89 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+
+/** 
+ * 7 - Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+ *    Dica: cada inicial termina com um ponto.
+*/
+
+const verifyDotsOnName = (element) => {
+  let countDotsOnName = 0;
+  const nameSplit = element.author.name.split('');
+  nameSplit.forEach((element) => {
+    if (element === '.') {
+      countDotsOnName += 1;
+    }
+  });
+  if (countDotsOnName === 3) return element;
+  return false;
+}
+
+function authorWith3DotsOnName(array) {
+  // escreva seu código aqui
+  const book = array.filter(verifyDotsOnName);
+  const nameBook = book.map((element) => element.name);
+  return nameBook[0];
+}
+
+console.log(authorWith3DotsOnName(books));
